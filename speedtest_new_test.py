09aa4b4489b09cc36e54f325dd67a19566382ff4
Tr4shL0rd <ketil04@gmail.com>
2022-10-04 20:50:10 +0200

    old function broke after around 40 attempts

--- speedtest_new_test.py
+++ speedtest_new_test.py
@@ -10,25 +10,20 @@ def convert_size(size_bytes):
    s = round(size_bytes / p, 2)
    return "%s %s" % (s, "Mbps")
 
-def attempt(attemps:int=0, Try:bool=True, max_attempt_limit=5, totalAttemps=0):
-    if Try:
-        print("Connecting...")
-        print(f"DEBUG: attempt #{totalAttemps}")
+def attempt(attemps:int=0, Try:bool=True, max_attempt_limit=5, totalAttemps=1):
+    connected=False
+    while not connected:
+        print("Connecting")
         try:
-            while not speedtest.Speedtest():
-                pass
+            st = speedtest.Speedtest()
         except speedtest.ConfigRetrievalError:
-            totalAttemps+=1
-            if attemps < max_attempt_limit:
-                attemps+=1
-            else:
-                attemps = max_attempt_limit
-            print(f"Connection Failed...\nTrying again in {attemps} seconds.\n")
-            time.sleep(attemps)
-            attempt(attemps,totalAttemps=totalAttemps)
-    return speedtest.Speedtest()
+            connected = False
+        connected = True
+        print("CONNECTED")
+        break
+    return st
     
-st = attempt(totalAttemps=0)
+st = attempt()
 servers = st.get_best_server()
 print("Checking Down Speeds...")
 down = st.download(threads=None)
