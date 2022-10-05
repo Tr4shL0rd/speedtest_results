d9e4f914ab14db6243e074ede7483bf215696815
Tr4shL0rd <ketil04@gmail.com>
2022-10-05 11:52:20 +0200

    formatting

--- speedtest_new_test.py
+++ speedtest_new_test.py
@@ -5,11 +5,11 @@ import math
 import bitmath
 
 def convert_size(size_bytes):
-   #size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-   i = int(math.floor(math.log(size_bytes, 1024)))
-   p = math.pow(1024, i)
-   s = round(size_bytes / p, 2)
-   return "%s %s" % (s, "Mbps")
+    #size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return "%s %s" % (s, "Mbps")
 
 def attempt(attemps:int=1,sleep_time:int=1, Try:bool=True, max_attempt_limit=5, totalAttemps=1, debug_sleep:int=0, debug_flag:bool=False):
     # pretty sure it breaks if connection is actually reached after n amount of retries 
