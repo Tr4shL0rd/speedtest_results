dcdf26e3ed5daff0453cb8b36e51278cf4af8203
Tr4shL0rd <ketil04@gmail.com>
2022-10-05 14:10:51 +0200

    checks for debug statements in production code

--- tools/debug_checker.py
+++ tools/debug_checker.py
@@ -1,8 +1,19 @@
-with open("speedtest_new_test.py", "r") as f:
-    lines = f.readlines()
-    Lines = []
-    for line in lines:
-        Lines.append(line.replace("\n", ""))
-    for line in Lines:
-        if "debug" in line.lower():
-            print(f"DEBUG STATEMENT AT LINE {Lines.index(line)+1}: {Lines[Lines.index(line)]} ")
+from rich import print as rprint
+from rich.console import Console
+from get_files import get_files
+def main():
+    files = get_files()
+    console = Console()
+    for file in files:
+        with open(file,"r") as f:
+            lines = f.readlines()
+            Lines = []
+            for line in lines:
+                Lines.append(line.replace("\n", ""))
+            for line in Lines:
+                if "debug" in line.lower():
+                    pre_debug = Lines[Lines.index(line)].split('debug')                        
+                    pretty_line = f"{pre_debug[0]}[red underline]debug{pre_debug[1]}[/red underline]"
+                    console.print(f"DEBUG STATEMENT AT {f.name.rsplit('/',1)[1]}:{Lines.index(line)+1}: {pretty_line} ",highlight=False)
+if __name__ == "__main__":
+    main()
